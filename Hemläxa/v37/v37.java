f6feb424047feaa7626324d2be9076fd7a36fb9f
RankoRan <ranko.ranilovic@elev.ga.ntig.se>
2019-09-15 18:57:25 +0200

    Har skrivit ut information om användaren

--- Hemläxa/v37/v37.java
+++ Hemläxa/v37/v37.java
@@ -6,21 +6,33 @@ public class v37{
 		Scanner scan = new Scanner(System.in);
 	
 		System.out.println("Vad heter du?");
-		String name = scan.next();
+		String namn = scan.next();
+		namn += scan.nextLine();
 		
 		System.out.println("Hur gammal är du?");
-		String age = scan.next();
+		String ålder = scan.next();
+		ålder += scan.nextLine();
 		
 		System.out.println("Vad är din adress");
 		String adress = scan.next();
+		adress += scan.nextLine();
 		
 		System.out.println("Vad är ditt postnummer?");
 		String postnummer = scan.next();
+		postnummer += scan.nextLine();
 		
 		System.out.println("Vilken stad bor du i?");
 		String stad = scan.next();
 		
 		System.out.println("Vad är ditt telefonnummer?");
 		String telefon = scan.next();
+		
+		System.out.println("           ");
+		System.out.println("Informaton:");
+		System.out.println("Namn:    " + namn + "");
+		System.out.println("Ålder:   " + ålder + "");
+		System.out.println("Adress:  " + adress + "");
+		System.out.println("         "+ postnummer + " " + stad + "");
+		System.out.println("Telefon: " + telefon + "");
 	}
 }
