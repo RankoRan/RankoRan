462be9315531ecb89d0ccb70294d38a46ddbf7ab
RankoRan <ranko.ranilovic@elev.ga.ntig.se>
2019-09-15 18:05:23 +0200

    Har skapat en scanner och frågat om användarens namn

--- Hemläxa/v37/v37.java
+++ Hemläxa/v37/v37.java
@@ -1,10 +1,13 @@
 import java.util.Scanner;
 
 public class v37{
-	public static void main(String[] args)
-	{
+	public static void main(String[] args){
+		
+		Scanner scan = new Scanner(System.in);
 	
-	
-	
+		System.out.println("Vad heter du?");
+		String name = scan.next();
+		
+		
 	}
 }
