e9fd1d96f5f32e6f94030769064363e74f3fa7db
RankoRan <ranko.ranilovic@elev.ga.ntig.se>
2019-10-07 10:35:06 +0200

    Läxa v.40 Spavanac

---
+++ Hemläxa/v.40/v40.java
@@ -0,0 +1,30 @@
+
+import java.util.Scanner;
+
+public class v40 {
+
+	public static void main(String[] args) {
+
+		Scanner scan = new Scanner(System.in);
+		
+		System.out.println("Skriv timmar och sen minuter i 24 timmarsklocka");
+		int H = scan.nextInt();
+		int M = scan.nextInt();
+		
+		M -= 45;
+		
+		if(M < 0)
+		{	
+			H -=1;
+			M +=60;
+		}
+	
+		if(H < 0)
+		H = 23;
+		
+		System.out.println(H + " " + M);
+		
+		
+	}
+
+}
