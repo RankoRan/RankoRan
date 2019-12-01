9fa776b610627b3d0817ab10df69acb35fd510e1
RankoRan <ranko.ranilovic@elev.ga.ntig.se>
2019-12-01 17:22:48 +0100

    Har gjort metod 16

--- Hemläxa/src/Uppgift.java
+++ Hemläxa/src/Uppgift.java
@@ -26,6 +26,7 @@ public class Uppgift {
 		System.out.println(svtDistance(10,5));
 		System.out.println(svtTime(10,2));
 		System.out.println(work(50,10));
+		System.out.println(power(1000,2));
 }
 
 	public static double fahrenheitToCelsius(double fahrenheit) {
@@ -132,5 +133,12 @@ public class Uppgift {
 		double work = force*distance;
 		
 		return work;
+	}
+	
+	public static double power(double work, double time) {
+		
+		double power = work/time;
+		
+		return power;
 	}
 }
